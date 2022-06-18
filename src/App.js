81254795ac125211d51107ddca32444895b2c212
Amit Dnyandeo Haral <amitharal04@gmail.com>
2022-06-18 23:16:52 +0530

    FirstCommit - Created React Project And Some Components

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Home from "./component/home-component/Home";
+import About from "./component/about-component/About";
+import Settings from "./component/settings-component/Settings";
+
+function App() {
+  return (
+    <div>
+      <Home />
+      <About />
+      <Settings />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/settings-component/Settings.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Settings extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Settings Component</h1>
+      </div>
+    );
+  }
+}
