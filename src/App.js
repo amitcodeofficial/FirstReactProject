85ff679e066638b6f4cf740a07320cb1d832dc81
Amit Dnyandeo Haral <amitharal04@gmail.com>
2022-06-19 21:48:11 +0530

    Modified - React Routing Implementation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
 import "./App.css";
-import Home from "./component/home-component/Home";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import About from "./component/about-component/About";
+import Home from "./component/home-component/Home";
 import Settings from "./component/settings-component/Settings";
 
 function App() {
   return (
-    <div>
-      <Home />
-      <About />
-      <Settings />
-    </div>
+    <Router>
+      <div class="app">
+        <h1>Hii App Component Here</h1>
+        <div>
+          <div>
+            <Link to="/about">About</Link>
+          </div>
+          <div>
+            <Link to="/home">Home</Link>
+          </div>
+          <div>
+            <Link to="/settings">Settings</Link>
+          </div>
+        </div>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/settings">
+            <Settings />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+{
+  /* <div>
+<Home />
+<About />
+<Settings />
+</div> */
+}
